d8b92f4f8b8a942eddf821c72fe0132416bc875c
VaishnaviSettu <vaishusandy23@outlook.com>
2025-05-06 20:49:03 +0530

    Update Pattern13.cpp

--- Week2/Pattern13.cpp
+++ Week2/Pattern13.cpp
@@ -5,10 +5,12 @@ int main() {
     int n;
     cout << "Enter n value: ";
     cin >> n;
+    int count = 1;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++) {
-            cout << i << " ";
+            cout << count <<" ";
         }
+        count++;
         cout<<endl;
     }
     return 0;
